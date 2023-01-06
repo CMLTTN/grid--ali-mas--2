5c988af6c92090078a762e431830239e1953cde1
CMLTTN <cemaldim@gmail.com>
2023-01-06 20:55:58 +0300

    first commit

---
+++ team.js
@@ -0,0 +1,13 @@
+// let a = [12,14,16]
+// let b = [11,13,17]
+// let c = []
+// c = a.concat(b)
+// console.log(c);
+
+// // c.sort((a,b)=>{
+// //     return
+
+// //  a - b ;
+// // });
+// C.sort((a,b) => a-b) 
+// console.log(c);
